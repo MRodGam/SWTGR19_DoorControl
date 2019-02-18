a32f98ae126e50cc3ef659f835813118accffed0
Matilde Gammelgaard <matildegammelgaard@hotmail.com>
2019-02-18 12:12:37 +0100

    Testcases startet

--- DoorControl/DoorControl.Test.Unit/DoorControl_Test_Unit.cs
+++ DoorControl/DoorControl.Test.Unit/DoorControl_Test_Unit.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DoorControl;
+using NUnit;
+using NUnit.Framework;
 
 namespace DoorControl.Test.Unit
 {
+    [TestFixture]
     class DoorControl_Test_Unit
     {
+        private DoorControl _uut;
+        public IDoor _door;
+        private IUserValidation _userValidation;
+        private IEntryNotification _entryNotification;
+        private IAlarm _alarm;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _door = new MockDoor();
+            _userValidation = new MockUserValidation();
+            _entryNotification = new MockEntryNotification();
+            _alarm = new MockAlarm();
+
+            _uut = new DoorControl(_door, _userValidation,_entryNotification,_alarm );
+        }
+
+        //[Test]
+        //public void RequestEntryCalled_ExpectedTrue()
+        //{
+        //    _uut.RequestEntry(1);
+        //    Assert.That(_userValidation.WasRequestCalled == true);
+        //}
     }
 }
